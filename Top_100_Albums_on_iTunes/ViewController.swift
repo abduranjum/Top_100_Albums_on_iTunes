d69952135de963dd0b889e6aecf75075318129c5
Abdurrahman Mubeen Ali <abdur.anjum@gmail.com>
2020-05-04 18:46:26 -0400

    Initial Commit

---
+++ Top_100_Albums_on_iTunes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top_100_Albums_on_iTunes
+//
+//  Created by Abdurrahman Ali on 5/4/20.
+//  Copyright © 2020 Abdurrahman Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Top_100_Albums_on_iTunesTests/Top_100_Albums_on_iTunesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Top_100_Albums_on_iTunesTests.swift
+//  Top_100_Albums_on_iTunesTests
+//
+//  Created by Abdurrahman Ali on 5/4/20.
+//  Copyright © 2020 Abdurrahman Ali. All rights reserved.
+//
+
+import XCTest
+@testable import Top_100_Albums_on_iTunes
+
+class Top_100_Albums_on_iTunesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
