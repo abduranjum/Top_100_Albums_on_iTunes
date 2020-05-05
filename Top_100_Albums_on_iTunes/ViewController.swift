1818e085b53f8368199cc10d248d2e83db14d40d
Abdurrahman Mubeen Ali <abdur.anjum@gmail.com>
2020-05-05 01:20:20 -0400

    [FEATURE] Implemented the server call to fetch the top 100 albums on iTunes.

--- Top_100_Albums_on_iTunes/ViewController.swift
+++ Top_100_Albums_on_iTunes/ViewController.swift
@@ -10,10 +10,37 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    var albums: [AnyObject]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        title = "Top 100 iTune Albums"
+        title = "Top 100 Albums on iTunes"
+        fetchTop100AlbumsOnItunes()
+    }
+    
+    func fetchTop100AlbumsOnItunes() {
+        let urlString = "https://rss.itunes.apple.com/api/v1/us/apple-music/coming-soon/all/100/explicit.json"
+        guard let url = URL(string: urlString) else { return }
+        let dataTask = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let feedData = data else { return }
+            do {
+                let json = try JSONSerialization.jsonObject(with: feedData) as? Dictionary<String, AnyObject>
+                
+                guard let feed = json?["feed"] as? Dictionary<String, AnyObject> else { return }
+                guard let results = feed["results"] as? [AnyObject] else { return }
+
+                self.albums = results
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            } catch {
+                print("Error: The JSON could not be parsed.")
+            }
+        }
+        
+        dataTask.resume()
     }
 }
 
